46a2765b7c622a4ade367b64505271afedd321e2
Lalit Kumar Annadevara <lalitkumar.a@gmail.com>
2019-12-08 03:49:05 +0000

    maps code fixed

--- map/views.py
+++ map/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from sightings.models import Sight
+from django.template import loader
 def index(request):
     sightings = Sight.objects.all()
-    template = loader.get_template('maps/maps.html')
+    template = loader.get_template('map/maps.html')
     context = {'sightings':sightings}
     return HttpResponse(template.render(context, request))
